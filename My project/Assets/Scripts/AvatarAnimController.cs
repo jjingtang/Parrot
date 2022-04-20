5c1094c0445105bb0cb08b114ad303ff0b95dd2a
Yueyuan54 <u3226957@uni.canberra.edu.au>
2022-04-20 22:26:53 +1000

    Update AvatarAnimController.cs
    update animator controller
    still can not linking to value output from Wit.ai

--- My project/Assets/Scripts/AvatarAnimController.cs
+++ My project/Assets/Scripts/AvatarAnimController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class AvatarAnimController : MonoBehaviour
 {
-    //access to the avatar
+    //access to the avatar allow us to modify though unity interface
     [SerializeField] GameObject avatar;
+    //access to animator of this game object
     private Animator anim;
     
 
@@ -24,7 +25,8 @@ public class AvatarAnimController : MonoBehaviour
 
     public void Greeting(string[] values)
     {
-        if(values[0] == "true")
+        //if the value from wit is true, set the anim ¡°isGreeting¡± to true so the animator plays waving animation
+        if (values[0] == "true")
         {
             anim.SetBool("isGreeting",true);
         }
